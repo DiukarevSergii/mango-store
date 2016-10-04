3d318a5be958c5efbc9fbb685991c88bf5b3ae1d
DiukarevSergii <vmesteprotiv@gmail.com>
2016-10-04 19:27:40 +0300

    add @Transaction

--- src/main/java/ua/com/mangostore/service/impl/CustomerServiceImpl.java
+++ src/main/java/ua/com/mangostore/service/impl/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package ua.com.mangostore.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.com.mangostore.entity.Customer;
 import ua.com.mangostore.repository.CustomerRepository;
 import ua.com.mangostore.service.CustomerService;
@@ -15,22 +16,26 @@ public class CustomerServiceImpl implements CustomerService{
     private CustomerRepository customerRepository;
 
     @Override
+    @Transactional
     public Customer addCustomer(Customer customer) {
         Customer saveCustomer = customerRepository.saveAndFlush(customer);
         return saveCustomer;
     }
 
     @Override
+    @Transactional
     public void delete(long id) {
         customerRepository.delete(id);
     }
 
     @Override
+    @Transactional
     public Customer getByName(String name) {
         return customerRepository.findByName(name);
     }
 
     @Override
+    @Transactional
     public Customer editCustomer(Customer customer) {
         return customerRepository.saveAndFlush(customer);
     }
