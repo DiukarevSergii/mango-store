c22f1c923970c9b2d78fca5b402f00e2f4d002ff
Diukarev Sergii <vmesteprotiv@gmail.com>
2016-12-02 18:07:45 +0200

    add comments

--- src/main/java/ua/com/mangostore/entity/Delivery.java
+++ src/main/java/ua/com/mangostore/entity/Delivery.java
@@ -3,38 +3,80 @@ package ua.com.mangostore.entity;
 import ua.com.mangostore.entity.enums.DeliveryType;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
+/**
+ * Класс описывает сущность "Доставка".
+ * Аннотация @Entity говорит о том что объекты этого класса будет обрабатываться Hibernate.
+ * Аннотация @Table(name = "Deliveries") указывает на таблицу "Deliveries", в которой будут храниться объекты.
+ *
+ * @author Diukarev Sergii
+ */
 @Entity
 @Table(name = "Deliveries")
 public class Delivery {
+
+    /**
+     * Уникальный код обьекта.
+     * Аннотация @Id говорит о том что поле является ключем для текущего объекта,
+     * Аннотация @GeneratedValue говорит о том что значение генерируется автоматически.
+     * Значение поля сохраняется в колонке "DELIVERY_ID".
+     */
     @Id
     @GeneratedValue
     @Column(name = "DELIVERY_ID")
     private long deliveryId;
 
+    /**
+     * Дата доставки
+     */
     @Column(name = "DELIVERY_DATE")
     private String deliveryDate;
 
+    /**
+     * Время доставки
+     */
     @Column(name = "DELIVERY_TIME")
     private String deliveryTime;
 
+    /**
+     * Стоимость доставки
+     */
     @Column(name = "DELIVERY_COST")
     private double cost;
 
+    /**
+     * Тип доставки
+     */
     @Column(name = "DELIVERY_TYPE")
     @Enumerated(EnumType.STRING)
     private DeliveryType deliveryType;
 
+    /**
+     * Заказ, который звязан с текущей доставкой.
+     * Значение поля (orderId объекта Order) сохраняется в колонке "ORDER_ID".
+     * в объекте класса {@link Order}.
+     * Между объектами классов {@link Order} и {@link Delivery} связь один-к-одному,
+     * а именно каждая запись в одной таблице напрямую связана с отдельной записью в другой таблице.
+     * Сущности связаны полностью каскадным обновлением записей в базе данных.
+     */
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
+    /**
+     * Сотрудник-курьер связанный с заказом.
+     * Значение поля (employeeId объекта Employee) сохраняется в колонке "EMPLOYEE_ID".
+     * Между объектами классов {@link Delivery} и {@link Employee} связь многие-к-одному,
+     * а именно много разных заказов могут иметь одного курьера.
+     * Выборка объекта status до первого доступа нему, при первом доступе к текущему объекту.
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "EMPLOYEE_ID")
     private Employee employee;
 
+    /**
+     * Конструктр без параметров.
+     */
     public Delivery() {
     }
 
@@ -99,7 +141,7 @@ public class Delivery {
         this.deliveryType = deliveryType;
     }
 
-    public void reset(){
+    public void reset() {
         this.deliveryTime = "";
         this.deliveryDate = "";
         this.setEmployee(null);
